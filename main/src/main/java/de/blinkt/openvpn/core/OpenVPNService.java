d7eb513fb9836fae5e00a202eae1179e7f759ba1
Josh Soref <jsoref@users.noreply.github.com>
2020-01-31 12:47:55 -0500

    spelling: building_configuration

--- main/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
+++ main/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
@@ -543,11 +543,11 @@ public class OpenVPNService extends VpnService implements StateListener, Callbac
 
 
         // Always show notification here to avoid problem with startForeground timeout
-        VpnStatus.logInfo(R.string.building_configration);
+        VpnStatus.logInfo(R.string.building_configuration);
 
         if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M  || (!foregroundNotificationVisible())) {
 
-            VpnStatus.updateStateString("VPN_GENERATE_CONFIG", "", R.string.building_configration, ConnectionStatus.LEVEL_START);
+            VpnStatus.updateStateString("VPN_GENERATE_CONFIG", "", R.string.building_configuration, ConnectionStatus.LEVEL_START);
             showNotification(VpnStatus.getLastCleanLogMessage(this),
                     VpnStatus.getLastCleanLogMessage(this), NOTIFICATION_CHANNEL_NEWSTATUS_ID, 0, ConnectionStatus.LEVEL_START, null);
         }
