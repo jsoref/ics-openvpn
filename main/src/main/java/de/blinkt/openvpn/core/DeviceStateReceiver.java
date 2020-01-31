86e8b0406c769310aeac9e3a4e9e49ebcd74bc2f
Josh Soref <jsoref@users.noreply.github.com>
2020-01-31 12:50:16 -0500

    spelling: shouldbeconnected

--- main/src/main/java/de/blinkt/openvpn/core/DeviceStateReceiver.java
+++ main/src/main/java/de/blinkt/openvpn/core/DeviceStateReceiver.java
@@ -262,7 +262,7 @@ public class DeviceStateReceiver extends BroadcastReceiver implements ByteCountL
 
         if (!netstatestring.equals(lastStateMsg))
             VpnStatus.logInfo(R.string.netstatus, netstatestring);
-        VpnStatus.logDebug(String.format("Debug state info: %s, pause: %s, shouldbeconnected: %s, network: %s ",
+        VpnStatus.logDebug(String.format("Debug state info: %s, pause: %s, shouldBeConnected: %s, network: %s ",
                 netstatestring, getPauseReason(), shouldBeConnected(), network));
         lastStateMsg = netstatestring;
 
