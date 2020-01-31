8ee5cafde443711d0acd7d5be512512f5a69993c
Josh Soref <jsoref@users.noreply.github.com>
2020-01-31 12:48:44 -0500

    spelling: getlatestdump

--- main/src/ui/java/de/blinkt/openvpn/activities/MainActivity.kt
+++ main/src/ui/java/de/blinkt/openvpn/activities/MainActivity.kt
@@ -47,7 +47,7 @@ class MainActivity : BaseActivity() {
             mPagerAdapter.addTab(R.string.graph, GraphFragment::class.java)
             mPagerAdapter.addTab(R.string.generalsettings, GeneralSettings::class.java)
             mPagerAdapter.addTab(R.string.faq, FaqFragment::class.java)
-            if (SendDumpFragment.getLastestDump(this) != null) {
+            if (SendDumpFragment.getLatestDump(this) != null) {
                 mPagerAdapter.addTab(R.string.crashdump, SendDumpFragment::class.java)
             }
 
