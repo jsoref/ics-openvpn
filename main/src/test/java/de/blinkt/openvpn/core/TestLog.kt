19976531d367784b6a3a28e10a262237e1cdf62c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-09-11 10:12:27 -0400

    spelling: marsh
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- main/src/test/java/de/blinkt/openvpn/core/TestLog.kt
+++ main/src/test/java/de/blinkt/openvpn/core/TestLog.kt
@@ -11,7 +11,7 @@ import org.junit.Test
 class TestLog {
 
     @Test
-    fun testMarschTooLong()
+    fun testMarshTooLong()
     {
         /* generate a string that is 16k long */
         var longtsring = "";
