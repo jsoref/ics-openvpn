75da9aa18ccd06db5b72d85f536877507af8584f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-09-11 08:58:32 -0400

    spelling: reciever
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- main/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
+++ main/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
@@ -663,7 +663,7 @@ public class OpenVPNService extends VpnService implements StateListener, Callbac
 
 
         /* we get an empty start request or explicitly get told to not replace the VPN then ignore
-         * a start request. This avoids OnBootreciver, Always and user quickly clicking to have
+         * a start request. This avoids OnBootReceiver, Always and user quickly clicking to have
          * weird race conditions
          */
         if (mProfile != null && mProfile == vp && (intent == null || noReplaceRequested))
