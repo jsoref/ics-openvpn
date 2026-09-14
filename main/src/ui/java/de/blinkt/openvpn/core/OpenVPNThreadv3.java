10dd2a759246805b6d94c30765301526af22fb5a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-09-14 00:16:39 -0400

    spelling: arbitrary
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- main/src/ui/java/de/blinkt/openvpn/core/OpenVPNThreadv3.java
+++ main/src/ui/java/de/blinkt/openvpn/core/OpenVPNThreadv3.java
@@ -344,7 +344,7 @@ public class OpenVPNThreadv3 extends ClientAPI_OpenVPNClient implements Runnable
     @Override
     public void sendAccMessage(AccMessage accMessage) {
         mHandler.post(() -> {
-            /* The C++ API here is a bit special in allowing a std::string with arbitary binary content */
+            /* The C++ API here is a bit special in allowing a std::string with arbitrary binary content */
             String message = new  String(accMessage.getMessage());
             send_app_control_channel_msg(accMessage.getProtocol(), message);
         });
